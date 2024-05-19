e15d6478c34e62e1e54abd3fa789901af0b9121c
Salgert <salgert@bsnx.net>
2024-05-19 22:19:14 +0200

    webserver

--- nxdisplay.h
+++ nxdisplay.h
@@ -6,6 +6,10 @@
 #include <Arduino.h>
 
 extern void display_start();
-extern void display_display()
+extern void display_display();
+
+extern void display_drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+extern void display_value(float value, int size, int x, int y);
+extern void display_str(String value, int size, int x, int y);
 
 #endif
